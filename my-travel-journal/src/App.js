21ee7976117dce673fe95ec9004a120a75ecdfa8
Jagdish Chaudhari <jagdish.chaudhari@1rivet.local>
2022-04-11 13:25:34 +0530

    feat: developed app for travel journal

--- my-travel-journal/src/App.js
+++ my-travel-journal/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./Card";
+import Header from "./Header";
 
-function App() {
+import { Data } from "./Data";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header />
+      <div className="page--content">
+        {Data.map((item) => {
+          return <Card key={item.id} travelData={item} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
